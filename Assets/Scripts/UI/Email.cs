b345bec966a9365c82fe3322a9dde15e3a8331b7
OrionGranatir <o.granatir@yahoo.com>
2016-07-31 16:47:11 -0700

    Rework game UI
    Rework main gameplay to look like a full screen desktop

--- Assets/Scripts/UI/Email.cs
+++ Assets/Scripts/UI/Email.cs
@@ -14,11 +14,19 @@ public class Email : MonoBehaviour
 	public Text subjectLine;
 	public Text body;
 	public Image document;
+	public Image icon;
+	private Mask mask;
 
+	private Vector3 defaultPos;
 	private Vector3 startingPos;
 	private Vector3 targetPos;
+	private float minY;
+	private float maxY;
+	private float xThreshold;
 
 	private bool showDocument = true;
+	private bool scrollingEnabled = false;
+
 	private const float timeToScale = 0.35f;
 	private float timeScaling = timeToScale;
 	private CEmail mEmail;
@@ -28,7 +36,28 @@ public class Email : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
-		startingPos = transform.position;
+		mask = GetComponentInParent<Mask>();
+
+		// Determine our min/max
+		RectTransform maskRect = mask.GetComponent<RectTransform>();
+		RectTransform documentRect = document.GetComponent<RectTransform>();
+		if( maskRect.rect.height < documentRect.rect.height )
+		{
+			scrollingEnabled = true;
+			minY = transform.position.y;
+			maxY = documentRect.rect.yMax + (documentRect.rect.height - maskRect.rect.height);
+		}
+		else
+		{
+			minY = transform.position.y;
+			maxY = transform.position.y;
+		}
+
+		// Determine movement left/right to change the email into an icon
+		xThreshold = maskRect.rect.width * 0.1f;
+
+		// Store our starting position
+		defaultPos = transform.position;
 		targetPos = startingPos;
 	}
 
@@ -58,68 +87,122 @@ public class Email : MonoBehaviour
 			}
 			transform.position = Vector3.Lerp( transform.position, targetPos, timeScaling / timeToScale );
 		}
-		else
-		{
-			transform.position = targetPos;
-		}
 	}
 
 	public void OnStartDrag()
 	{
-		ShowIcon();
+		defaultPos = transform.position;
+		startingPos = Input.mousePosition;
 	}
 
 	public void OnDrag()
 	{
+		// Update our position
 		targetPos = Input.mousePosition;
+		
+		// Check if we should change to the email icon
+		if( showDocument )
+		{
+			Vector3 delta = Input.mousePosition - startingPos;
+			float deltaX = delta.x;
+			float deltaY = delta.y; 
+
+			// Check if we should change to the email icon
+			float distanceFromCenter = Mathf.Abs( targetPos.x - startingPos.x );
+			if( distanceFromCenter > xThreshold )
+			{
+				ShowIcon();
+			}
+			else
+			{
+				// Clamp x to the middle
+				deltaX = 0.0f;
+			}
+
+			// Clamps y
+			deltaY = ( defaultPos.y + deltaY < minY ) ? minY - defaultPos.y : deltaY;
+			deltaY = ( defaultPos.y + deltaY > maxY ) ? maxY - defaultPos.y : deltaY;
+
+			// Allow us to scroll, only if enabled
+			if( !scrollingEnabled )
+			{
+				deltaX = 0.0f;
+				deltaY = 0.0f;
+			}
+
+			targetPos = defaultPos + new Vector3( deltaX, deltaY, 0.0f );
+		}
+		else
+		{
+			// Just follow the mouse position directly
+			targetPos = Input.mousePosition;
+		}
+
+		// Move us to the target position
+		transform.position = targetPos;
 	}
 		
 	public void OnEndDrag(BaseEventData data)
 	{
-		ShowDocument();
-
-		// Check where we have dropped...
-		PointerEventData eventData = (PointerEventData)data;
-		List<RaycastResult> hits = new List<RaycastResult>();
-		EventSystem.current.RaycastAll( eventData, hits );
-
-		bool foundTarget = false;
-		if( hits.Count > 0 )
+		if( !showDocument )
 		{
-			for( int index = 0; index < hits.Count; index++ )
+			ShowDocument();
+
+			// Check where we have dropped...
+			PointerEventData eventData = (PointerEventData)data;
+			List<RaycastResult> hits = new List<RaycastResult>();
+			EventSystem.current.RaycastAll( eventData, hits );
+
+			bool foundTarget = false;
+			if( hits.Count > 0 )
 			{
-				DropTarget target = hits[ index ].gameObject.GetComponent<DropTarget>();
-				if( target )
+				for( int index = 0; index < hits.Count; index++ )
 				{
-					if(CategoryChosenEvent != null)
+					DropTarget target = hits[ index ].gameObject.GetComponent<DropTarget>();
+					if( target )
 					{
-						CategoryChosenEvent(target.Category);
+						if(CategoryChosenEvent != null)
+						{
+							CategoryChosenEvent(target.Category);
+						}
+						target.RecieveEmail( this );
+						foundTarget = true;
+						break;
 					}
-					target.RecieveEmail( this );
-					foundTarget = true;
-					break;
 				}
 			}
-		}
 
-		// Check if we have found a target
-		if( foundTarget )
-		{
-			// Destory ourselves
-			Destroy( gameObject );
+			// Check if we have found a target
+			if( foundTarget )
+			{
+				// Destory ourselves
+				Destroy( gameObject );
+			}
 		}
 	}
 
 	private void ShowIcon()
 	{
 		showDocument = false;
+		icon.gameObject.SetActive( true );
+
+		// Set up animation
 		timeScaling = 0.0f;
+
+		// Disable the mask so we can move out of the email panel
+		mask.enabled = false;
 	}
 
 	private void ShowDocument()
 	{
 		showDocument = true;
-		targetPos = startingPos;
+		icon.gameObject.SetActive( false );
+
+		// Set up animation
+		targetPos = defaultPos;
 		timeScaling = 0.0f;
+
+		// Enable the mask so we are only show in the email panel
+		mask.enabled = true;
 	}
 }
